84a6961c59f1e4b50a45403a73a9d8034b4e68f3
lon21 <lon21@lon21.pl>
2026-05-20 21:23:03 +0200

    packwiz refresh

--- kubejs/startup_scripts/register/blocs/custom_barrier_block.js
+++ kubejs/startup_scripts/register/blocs/custom_barrier_block.js
@@ -2,11 +2,16 @@ StartupEvents.registry('block', event => {
 	event.create('custom_barrier')
 		.displayName('Custom Barrier')
 		.noSoundType()
-        .unbreakable()
+		.unbreakable()
 		.resistance(6000000)
 		.noDrops()
-        .opaque(false)
-        .renderType('translucent')
+		.opaque(false)
+		.renderType('translucent')
 		.notSolid()
-		.transparent(true);
+		.transparent(true)
+		.customBlockProperties(properties => {
+			properties.skipRendering((state, adjacentState, direction) => {
+				return adjacentState.is(state.getBlock());
+			});
+		});;
 });
