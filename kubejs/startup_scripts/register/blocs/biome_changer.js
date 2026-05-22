2187a212a3f6ddeed3c74b32e0c0f80941eac157
lon21 <lon21@lon21.pl>
2026-05-22 15:32:41 +0200

    packwiz refresh

---
+++ kubejs/startup_scripts/register/blocs/biome_changer.js
@@ -0,0 +1,8 @@
+StartupEvents.registry('block', event => {
+	event.create('biome_changer')
+		.displayName('Biome Changer')
+		.resistance(200)
+		.requiresTool()
+		.tagBlock('#minecraft:mineable/pickaxe')
+		.tagBlock('#minecraft:needs_iron_tool')
+});
